4ca45cf7cfa45dcc843360e612dfa275580ba384
FjrREPO <jossfajar21@gmail.com>
2024-07-02 08:49:25 +0700

    p

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode, useState } from "react";
 import { ThemeProvider } from "./theme-provider";
 import { SpotifyProvider } from "./spotify-provider";
-import { MusicPlayerProvider } from "./music-provider";
 
 interface Props {
     children: ReactNode;
@@ -23,16 +22,14 @@ const Providers = (props: Props) => {
     return (
         <SpotifyProvider>
             <QueryClientProvider client={client}>
-                <MusicPlayerProvider>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="dark"
-                        enableSystem
-                        disableTransitionOnChange
-                    >
-                        {props.children}
-                    </ThemeProvider>
-                </MusicPlayerProvider>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {props.children}
+                </ThemeProvider>
             </QueryClientProvider>
         </SpotifyProvider>
     )
